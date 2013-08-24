399402ff7fd0d4d370fe15e2501d220e8232a4a4
devhong <jon.hong@qq.com>
2013-08-24 16:57:57 +0800

    init

---
+++ Riak.Driver/RiakObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Riak.Driver
+{
+    /// <summary>
+    /// riak object
+    /// </summary>
+    public sealed class RiakObject
+    {
+        /// <summary>
+        /// new
+        /// </summary>
+        /// <param name="bucket"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public RiakObject(string bucket, string key, string value)
+            : this(bucket, key, Encoding.UTF8.GetBytes(value))
+        {
+        }
+        /// <summary>
+        /// new
+        /// </summary>
+        /// <param name="bucket"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public RiakObject(string bucket, string key, byte[] value)
+        {
+
+        }
+    }
+}
