933f8555625d35f2f078be346c5ed53893c55641
洪家旺 <jon.hong@qq.com>
2013-08-25 22:24:06 +0800

    update

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,21 +9,36 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            var riakClient = new Riak.Driver.RiakClient();
-            riakClient.RegisterServerNode("1", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.70"), 8087));
-            riakClient.RegisterServerNode("2", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.71"), 8087));
-            riakClient.RegisterServerNode("3", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.72"), 8087));
+            var riakSocketClient = new Riak.Driver.RiakSocketClient();
+            riakSocketClient.RegisterServerNode("1", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.70"), 8087));
+            riakSocketClient.RegisterServerNode("2", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.71"), 8087));
+            riakSocketClient.RegisterServerNode("3", new System.Net.IPEndPoint(System.Net.IPAddress.Parse("10.0.20.72"), 8087));
 
-            int callback = 0;
-            int error = 0;
-            for (int i = 0; i < 1000; i++)
+            var riakClient = new Riak.Driver.RiakClient(riakSocketClient);
+
+            riakClient.Put(new Riak.Driver.Messages.RpbPutReq
             {
-                riakClient.Put("bucket1", "test1" + i.ToString(), "hello riak!" + i.ToString()).ContinueWith(c =>
+                bucket = Encoding.UTF8.GetBytes("bucket1"),
+                key = Encoding.UTF8.GetBytes("key1"),
+                content = new Riak.Driver.Messages.RpbContent
                 {
-                    if (c.IsFaulted) Console.Title = ("error" + System.Threading.Interlocked.Increment(ref error).ToString());
-                    else Console.WriteLine("ok" + System.Threading.Interlocked.Increment(ref callback).ToString());
-                });
-            }
+                    value = BitConverter.GetBytes(DateTime.UtcNow.Ticks)
+                }
+            }).ContinueWith(c =>
+            {
+                if (c.IsFaulted) Console.WriteLine(c.Exception.ToString());
+                else Console.WriteLine(Encoding.UTF8.GetString(c.Result.vclock));
+            });
+
+            riakClient.Get(new Riak.Driver.Messages.RpbGetReq
+            {
+                bucket = Encoding.UTF8.GetBytes("bucket1"),
+                key = Encoding.UTF8.GetBytes("key1")
+            }).ContinueWith(c =>
+            {
+                if (c.IsFaulted) Console.WriteLine(c.Exception.ToString());
+                else Console.WriteLine(BitConverter.ToInt64(c.Result.content[0].value, 0));
+            });
 
             Console.ReadLine();
         }
--- Riak.Driver/RiakClient.cs
+++ Riak.Driver/RiakClient.cs
@@ -1,157 +1,60 @@
 ﻿using System;
-using System.Text;
 using System.Threading.Tasks;
-using Sodao.FastSocket.Client;
-using Sodao.FastSocket.SocketBase;
+using Riak.Driver.Messages;
 
 namespace Riak.Driver
 {
     /// <summary>
     /// riak client
     /// </summary>
-    public sealed class RiakClient : PooledSocketClient<RiakResponse>
+    public sealed class RiakClient
     {
         #region Private Members
-        private RiakServerPool _serverPool = null;
+        private readonly RiakSocketClient _socket = null;
         #endregion
 
         #region Constructors
         /// <summary>
         /// new
         /// </summary>
-        public RiakClient()
-            : this(8192, 8192, 3000, 3000)
+        /// <param name="socket"></param>
+        public RiakClient(RiakSocketClient socket)
         {
-        }
-        /// <summary>
-        /// new
-        /// </summary>
-        /// <param name="socketBufferSize"></param>
-        /// <param name="messageBufferSize"></param>
-        /// <param name="millisecondsSendTimeout"></param>
-        /// <param name="millisecondsReceiveTimeout"></param>
-        public RiakClient(int socketBufferSize,
-            int messageBufferSize,
-            int millisecondsSendTimeout,
-            int millisecondsReceiveTimeout)
-            : base(new RiakPbProtocol(), socketBufferSize, messageBufferSize, millisecondsSendTimeout, millisecondsReceiveTimeout)
-        {
+            if (socket == null) throw new ArgumentNullException("socket");
+            this._socket = socket;
         }
         #endregion
 
-        #region Override Methods
-        /// <summary>
-        /// init server pool
-        /// </summary>
-        /// <returns></returns>
-        protected override IServerPool InitServerPool()
-        {
-            return this._serverPool = new RiakServerPool(this);
-        }
-        /// <summary>
-        /// OnServerPoolServerAvailable
-        /// </summary>
-        protected override void OnServerPoolServerAvailable()
-        {
-            var request = base.DequeueFromPendingQueue();
-            if (request == null) return;
-            base.Send(request);
-        }
-        /// <summary>
-        /// OnStartSending
-        /// </summary>
-        /// <param name="connection"></param>
-        /// <param name="packet"></param>
-        protected override void OnStartSending(IConnection connection, Packet packet)
-        {
-            connection.UserData = packet;
-            packet.Tag = connection;
-            base.OnStartSending(connection, packet);
-        }
-        /// <summary>
-        /// OnResponse
-        /// </summary>
-        /// <param name="connection"></param>
-        /// <param name="response"></param>
-        protected override void OnResponse(IConnection connection, RiakResponse response)
-        {
-            var packet = connection.UserData as Packet;
-            if (packet != null) packet.Tag = null;
-            connection.UserData = null;
-
-            //try send next request from pending queue.
-            var request = base.DequeueFromPendingQueue();
-            if (request == null) { this._serverPool.Release(connection); return; };
-            connection.BeginSend(request);
-        }
-        #endregion
-
-        #region Private Methods
-        /// <summary>
-        /// execute
-        /// </summary>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="cmdName"></param>
-        /// <param name="payload"></param>
-        /// <param name="funResult"></param>
-        /// <param name="asyncState"></param>
-        /// <returns></returns>
-        private Task<TResult> Execute<TResult>(string cmdName, byte[] payload, Func<RiakResponse, TResult> funResult, object asyncState = null)
-        {
-            var source = new TaskCompletionSource<TResult>();
-
-            Request<RiakResponse> request = null;
-            request = new Request<RiakResponse>(base.NextRequestSeqID(), cmdName, payload, ex =>
-            {
-                var rex = ex as RequestException;
-                if (rex != null && rex.Error == RequestException.Errors.ReceiveTimeout)
-                {
-                    var connection = request.Tag as IConnection;
-                    if (connection != null) connection.BeginDisconnect();
-                }
-                source.TrySetException(ex);
-            },
-            response =>
-            {
-                TResult result;
-                try { result = funResult(response); }
-                catch (Exception ex) { source.TrySetException(ex); return; }
-                source.TrySetResult(result);
-            });
-
-            this.Send(request);
-            return source.Task;
-        }
-        #endregion
-
+        #region Put
         /// <summary>
         /// put
         /// </summary>
-        /// <param name="bucket"></param>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
+        /// <param name="req"></param>
         /// <param name="asyncState"></param>
         /// <returns></returns>
-        public Task Put(string bucket, string key, string value, object asyncState = null)
+        public Task<RpbPutResp> Put(RpbPutReq req, object asyncState = null)
         {
-            var req = new Messages.RpbPutReq
-            {
-                key = Encoding.UTF8.GetBytes(key),
-                bucket = Encoding.UTF8.GetBytes(bucket),
-                content = new Messages.RpbContent { value = Encoding.UTF8.GetBytes(value) }
-            };
-
-            byte[] bytes = null;
-            using (var ms = new System.IO.MemoryStream())
-            {
-                ms.Position = 5;
-                ProtoBuf.Serializer.Serialize(ms, req);
-                bytes = ms.ToArray();
-            }
-            bytes[4] = Messages.Codes.RpbPutReq;
-            Buffer.BlockCopy(Sodao.FastSocket.SocketBase.Utils.NetworkBitConverter.GetBytes(bytes.Length - 4), 0, bytes, 0, 4);
-
-            return Execute<bool>("put", bytes, c => true, asyncState);
+            var souce = new TaskCompletionSource<RpbPutResp>(asyncState);
+            this._socket.Execute<RpbPutReq, RpbPutResp>(Codes.RpbPutReq, req, ex => souce.TrySetException(ex),
+                res => souce.TrySetResult(res));
+            return souce.Task;
         }
+        #endregion
+
+        #region Get
+        /// <summary>
+        /// get
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="asyncState"></param>
+        /// <returns></returns>
+        public Task<RpbGetResp> Get(RpbGetReq req, object asyncState = null)
+        {
+            var souce = new TaskCompletionSource<RpbGetResp>(asyncState);
+            this._socket.Execute<RpbGetReq, RpbGetResp>(Codes.RpbGetReq, req, ex => souce.TrySetException(ex),
+                res => souce.TrySetResult(res));
+            return souce.Task;
+        }
+        #endregion
     }
 }
