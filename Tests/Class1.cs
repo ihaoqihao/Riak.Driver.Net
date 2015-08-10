4dfe7e88ec087891b3639a2ce748663d8de3e7d7
devhong <jon.hong@qq.com>
2015-08-10 15:18:04 +0800

    init

---
+++ Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Riak.Driver.Utils;
+using System;
+
+namespace Tests
+{
+    public class Class1
+    {
+        static public void Main()
+        {
+            System.Threading.ThreadPool.SetMinThreads(30, 30);
+            var riakClient = Riak.Driver.RiakClientPool.Get("riak.config", "riak1");
+
+            riakClient.Put(new Riak.Driver.RiakObject("bucket1", "key1", "value1"), options => options.SetW(1).SetReturnBody(true)).ContinueWith(c =>
+            {
+                if (c.IsFaulted) Console.WriteLine(c.Exception.ToString());
+                else Console.WriteLine(c.Result.Value.GetString());
+            });
+
+            riakClient.Get("bucket1", new string[] { "key1", "key2" }, options => options.SetR(1)).ContinueWith(c =>
+            {
+                if (c.IsFaulted) Console.WriteLine(c.Exception.ToString());
+                else Console.WriteLine(c.Result.Length);
+            });
+            Console.ReadLine();
+        }
+    }
+}
